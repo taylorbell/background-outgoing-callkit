aa90306dab46aa81a443b3598f81908561e83de9
Taylor Bell <taylorbell.home@gmail.com>
2019-10-10 10:34:10 -0500

    Initial Commit

---
+++ RemoteEventSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemoteEventSample
+//
+//  Created by Taylor Bell on 10/10/19.
+//  Copyright © 2019 Taylor Bell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
